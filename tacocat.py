66cd12521ea758177264836577f9e7f2b53932ca
axelsen <husmann@live.dk>
2017-07-27 21:43:20 +0200

    tacocat1

--- tacocat.py
+++ tacocat.py
@@ -1,7 +1,7 @@
-from flask import (Flask, g, render_template, flash, redirect, url_for, abort)
+from flask import Flask, g, render_template, flash, redirect, url_for
 from flask.ext.bcrypt import check_password_hash
-from flask.ext.login import (LoginManager, login_user, logout_user,
-                             login_required, current_user)
+from flask.ext.login import (LoginManager, login_user,
+                             logout_user, login_required, current_user)
 
 import forms
 import models
@@ -11,7 +11,7 @@ PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
-app.secret_key = 'adfaldv mz,æxc x,cxvzæxfldvmadæmk!!!!12+1249)"¤)"¤?="¤?='
+app.secret_key = 'youdontknowmysecretkey'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -19,55 +19,52 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(userid):
-    try:
-        return models.User.get(models.User.id == userid)
-    except models.DoesNotExist:
-        return None
-    
+  try:
+      return models.User.get(models.User.id == userid)
+  except models.DoesNotExist:
+      return None
+
 @app.before_request
 def before_request():
-    """Connect to the database before each request."""
-    g.db = models.DATABASE
-    g.db.connect()
-    g.user = current_user
-
+  """Connect to the database before each request."""
+  g.db = models.DATABASE
+  g.db.connect()
+  g.user = current_user
 
 @app.after_request
 def after_request(response):
-    """Close the database connection after each request."""
-    g.db.close()
-    return response
-
+  """Close the database connection after each request."""
+  g.db.close
+  return response
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
-    form = forms.RegisterForm()
-    if form.validate_on_submit():
-        flash("Yay, you registered!", "success")
-        models.User.create_user(
-            email=form.email.data,
-            password=form.password.data,
-        )
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
-
+  form = forms.RegisterForm()
+  if form.validate_on_submit():
+      flash("Yay, you registered!", "success")
+      models.User.create_user(
+          email = form.email.data,
+          password = form.password.data
+      )
+      return redirect(url_for('index'))
+  return render_template('register.html', form=form)
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    form = forms.LoginForm()
-    if form.validate_on_submit():
-        try:
-            user = models.User.get(models.User.email == form.email.data)
-        except models.DoesNotExist:
-            flash("Your email or password doesn't match!", "error")
+  form = forms.LoginForm()
+  if form.validate_on_submit():
+    try:
+        user = models.User.get(models.User.email == form.email.data)
+    except models.DoesNotExist:
+        flash("Email is incompatible", "error")
+    else:
+        if check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("You are logged in", "success")
+            return redirect(url_for('index'))
         else:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user)
-                flash("You've been logged in!", "success")
-                return redirect(url_for('index'))
-            else:
-                flash("Your email or password doesn't match!", "error")
-    return render_template('login.html', form=form)
+            flash("Password does not match", "error")
+  return render_template('login.html', form=form)
 
 @app.route('/logout')
 @login_required
@@ -76,40 +73,35 @@ def logout():
     flash("You've been logged out! Come back soon!", "success")
     return redirect(url_for('index'))
 
-
-@app.route('/taco', methods=("GET", "POST"))
+@app.route('/new_taco', methods=('GET','POST'))
 @login_required
 def taco():
     form = forms.TacoForm()
     if form.validate_on_submit():
-        flash("A taco has been born.", "success")
-        models.Taco.create(
-            protein=form.protein.data,
-            cheese=form.cheese.data,
-            shell=form.shell.data,
-            extras=form.extras.data,
-            user=g.user.id
-        )
+        models.Taco.create(user = g.user._get_current_object(),
+                           protein = form.protein.data,
+                           shell = form.shell.data,
+                           cheese = form.cheese.data,
+                           extras = form.extras.data.strip()
+                          )
+        flash("Your taco has been added!!", "success")
         return redirect(url_for('index'))
-    return redirect(url_for('index'))
-    taco_protein = type1, type2, type3, None
-    cheese = True, False
-    shell = wheat, corn, mysterious
-    
-    
+    return render_template('taco.html', form=form)
+
+
 @app.route('/')
 def index():
-    tacos = models.Taco.select().limit(10)
-    return render_template('index.html', tacos=tacos)
+  tacos = models.Taco.select().limit(10)
+  return render_template('index.html', tacos=tacos)
 
 if __name__== '__main__':
-    models.initialize()
-    try:
-        with models.DATABASE.transaction():
-            models.User.create_user(
-              email = 'billy@example.com',
-              password = 'password'
-            )
-    except ValueError:
-        pass
-    app.run(debug=DEBUG, host=HOST, port=PORT)
+  models.initialize()
+  try:
+    with models.DATABASE.transaction():
+        models.User.create_user(
+          email = 'billy@example.com',
+          password = 'password'
+        )
+  except ValueError:
+    pass
+  app.run(debug=DEBUG, host=HOST, port=PORT)
