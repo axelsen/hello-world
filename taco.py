b3e96e4031abb261c966ce6a2e17c69cc58dfb1e
axelsen <husmann@live.dk>
2017-07-28 22:01:26 +0200

    Update taco.py

--- taco.py
+++ taco.py
@@ -3,13 +3,13 @@
 def taco():
     form = forms.TacoForm()
     if form.validate_on_submit():
-            models.Taco.create(
-            protein=form.protein.data,
-            cheese=form.cheese.data,
-            shell=form.shell.data,
-            extras=form.extras.data,
-            user=g.user.id
-        )
+        models.Taco.create(
+        protein=form.protein.data,
+        cheese=form.cheese.data,
+        shell=form.shell.data,
+        extras=form.extras.data,
+        user=g.user.id
+    )
         flash("A taco has been born.", "success")
         return redirect(url_for('index'))
-    return redirect(url_for('index'))
+    return render_template('taco.html', form=form)
