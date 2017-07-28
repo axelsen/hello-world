829111d80bcb3d52e467688be4709f30af8f6331
axelsen <husmann@live.dk>
2017-07-28 22:00:42 +0200

    Create taco.py

---
+++ taco.py
@@ -0,0 +1,15 @@
+@app.route('/taco', methods=("GET", "POST"))
+@login_required
+def taco():
+    form = forms.TacoForm()
+    if form.validate_on_submit():
+            models.Taco.create(
+            protein=form.protein.data,
+            cheese=form.cheese.data,
+            shell=form.shell.data,
+            extras=form.extras.data,
+            user=g.user.id
+        )
+        flash("A taco has been born.", "success")
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
