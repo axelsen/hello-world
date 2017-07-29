67de7f9646e5149681a508012e5fb753b376a48a
axelsen <husmann@live.dk>
2017-07-29 21:40:08 +0200

    Create courses.py

---
+++ courses.py
@@ -0,0 +1,17 @@
+class CourseList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course URL provided',
+            location=['form', 'json'],
+            type=inputs.url
+        )
+        super().__init__()
